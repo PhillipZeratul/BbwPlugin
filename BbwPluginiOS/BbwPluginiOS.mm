c837c9c02142b5638d15ce498476f609bc50b40f
Phillip Zhang <phillipzhang@Phillips-MacBook-Pro.local>
2018-04-25 14:18:16 +0800

    Switched to Release and Removed Debug Infos

--- BbwPluginiOS/BbwPluginiOS.mm
+++ BbwPluginiOS/BbwPluginiOS.mm
@@ -7,7 +7,6 @@
 //
 
 #import "BbwPluginiOS.h"
-typedef Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> RowMatrixXd;
 
 extern "C" {
     
@@ -17,29 +16,14 @@ extern "C" {
                      int* ptr_BE, int beCount,
                      double* ptr_W)
     {
-        std::cout << "Testing BbwPluginIOS" << std::endl;
+        std::cout << "BbwPluginIOS" << std::endl;
         
-        // V : Vertices : std::vector<std::vector<double> > : numOfVertices * double[3] -> Convert from float[][2] to double [][3]
-        // F : Triangle : Index std::vector<std::vector<int> > : numOfTriangles * int[3] -> Use directly
-        /// dont need T : Tetrahedra : Index std::vector<std::vector<int> > : numOfTetrahedra * int[4]
-        // C : Bone Vertex Position : std::vector<std::vector<double> > : numOfVertices * double[3] -> Convert from float[][2] to double [][3]
-        // BE : Bone Edges Indices: std::vector<std::vector<int> > : numOfEdges * int[2] -> Use directly
-        // W : Weights Output
-
-        
-        
-        ///
-//        for (int i = 0; i < 6; i++)
-//            std::cout << "ptr_V[" << i << "] = " << ptr_V[i] << std::endl;
-//        for (int i = 0; i < 9; i++)
-//            std::cout << "ptr_F[" << i << "] = " << ptr_F[i] << std::endl;
-        for (int i = 0; i < 6; i++)
-            std::cout << "ptr_C[" << i << "] = " << ptr_C[i] << std::endl;
-        for (int i = 0; i < 6; i++)
-            std::cout << "ptr_BE[" << i << "] = " << ptr_BE[i] << std::endl;
-        ///
-        
-        
+        // A little confused with colMajor and rowMajor, so maybe not correct here.
+        // V  (Vertices)             : float[vCount, 2](rowMajor) -> double [vCount * 3](colMajor) -> Matrix<double, vCount, 3, colMajor>
+        // F  (Triangle Indices)     : int[fCount * 3](rowMajor)  -> int[fCount * 3](colMajor)     -> Matrix<int, fCount, 3, colMajor>
+        // C  (Bone Point Positions) : float[cCount, 2](rowMajor) -> double[cCount * 3](colMajor)  -> Matrix<double, cCount, 3, colMajor>
+        // BE (Bone Edge Indices)    : int[beCount, 2](rowMajor)  -> int[beCount * 2](colMajor)    -> Matrix<int, beCount, 2, colMajor>
+        // W  (Output Weights)       : double[beCount, oriVCount] <- double[beCount * oriVCount]   <- Matrix<double, oriCount, beCount, colMajor>
         
         double array_V[vCount * 3];
         for (int i = 0; i < vCount; i++)
@@ -69,18 +53,6 @@ extern "C" {
             array_BE[i + beCount] = ptr_BE[i * 2 + 1];
         }
         
-        
-        ///
-//        for (int i = 0; i < 9; i++)
-//            std::cout << "array_V[" << i << "] = " << array_V[i] << std::endl;
-//        for (int i = 0; i < 9; i++)
-//            std::cout << "array_F[" << i << "] = " << array_F[i] << std::endl;
-        for (int i = 0; i < 9; i++)
-            std::cout << "array_C[" << i << "] = " << array_C[i] << std::endl;
-        ///
-    
-        
-        
         Eigen::MatrixXd V, C, W;
         Eigen::MatrixXi T, F, BE;
         // List of boundary indices (aka fixed value indices into VV)
@@ -94,94 +66,27 @@ extern "C" {
         BE = Eigen::Map<Eigen::MatrixXi, Eigen::RowMajor>(&array_BE[0], beCount, 2);
         
         igl::boundary_conditions(V,F,C,Eigen::VectorXi(),BE,Eigen::MatrixXi(),b,bc);
-
-        ///
-        std::string sep = "\n----------------------------------------\n";
-        Eigen::IOFormat CleanFmt(4, 0, ", ", "\n", "[", "]");
-        //std::cout << "V " << V.format(CleanFmt) << sep;
-        //std::cout << "F " << F.format(CleanFmt) << sep;
-        std::cout << "C " << C.format(CleanFmt) << sep;
-        std::cout << "BE " << BE.format(CleanFmt) << sep;
-        std::cout << "b " << b.format(CleanFmt) << sep;
-        std::cout << "bc " << bc.format(CleanFmt) << sep;
-        ///
-        std::cout << "Flag 1" << std::endl;
-        
         
         // compute BBW weights matrix
         igl::BBWData bbw_data;
         // only a few iterations for sake of demo
         bbw_data.active_set_params.max_iter = 8;
         bbw_data.verbosity = 2;
-        //if(!igl::bbw(V,T,b,bc,bbw_data,W))
-        if(!igl::bbw(V,F,b,bc,bbw_data,W))
-        {
-            return EXIT_FAILURE;
-        }
         
-        std::cout << "Flag 2" << std::endl;
+        if(!igl::bbw(V,F,b,bc,bbw_data,W))
+            return EXIT_FAILURE;
         
         // Normalize weights to sum to one
-        //igl::normalize_row_sums(W,W);
-        
-        std::cout << "Flag 3" << std::endl;
-        
-        
-        ///
-        std::cout << "W " << W.format(CleanFmt) << sep;
-        ///
-        std::cout << "V[" << V.rows() << ", " << V.cols() << "] " << vCount << std::endl;
-        std::cout << "F[" << F.rows() << ", " << F.cols() << "] " << fCount << std::endl;
-        std::cout << "C[" << C.rows() << ", " << C.cols() << "] " << cCount << std::endl;
-        std::cout << "BE[" << BE.rows() << ", " << BE.cols() << "] " << beCount << std::endl;
-        std::cout << "b[" << b.rows() << ", " << b.cols() << "] " << std::endl;
-        std::cout << "bc[" << bc.rows() << ", " << bc.cols() << "] " << std::endl;
-        std::cout << "W[" << W.rows() << ", " << W.cols() << "] " << beCount << " * " << vCount << std::endl;
-        ///
+        // It seems that weights does not need to sum to one in Anima2D
+        //igl::normalize_col_sums(W,W);
         
         double array_W[W.rows() * W.cols()];
         Eigen::Map<Eigen::MatrixXd>(array_W, W.rows(), W.cols()) = W;
         
-        
-        ///
-        for(int i = 0; i < 9; i++)
-            std::cout << "array_W[" << i << "] = " << array_W[i] << std::endl;
-        ///
-        
         for (int i = 0; i < oriVCount; i++)
-        {
             for (int j = 0; j < beCount; j++)
-            {
                 ptr_W[i + oriVCount * j] = array_W[i + vCount * j];
-            }
-        }
+        
         return 0;
     }
-    
-//    int BbwPluginIOS(float* ptr_V, int vCount, int oriVCount,
-//                     int* ptr_F, int fCount,
-//                     float* ptr_C, int cCount,
-//                     int* ptr_BE, int beCount,
-//                     float* ptr_W)
-//    {
-//        std::cout << "Testing BbwPluginIOS" << std::endl;
-//
-//        for (int i = 0; i < vCount * 2; i++)
-//            std::cout << "ptr_V[" << i << "] = " << ptr_V[i] << std::endl;
-//        std::cout << "oriVCount = " << oriVCount << std::endl;
-//        for (int i = 0; i < fCount; i++)
-//            std::cout << "ptr_F[" << i << "] = " << ptr_F[i] << std::endl;
-//        for (int i = 0; i < cCount * 2; i++)
-//            std::cout << "ptr_C[" << i << "] = " << ptr_C[i] << std::endl;
-//        for (int i = 0; i < beCount * 2; i++)
-//            std::cout << "ptr_BE[" << i << "] = " << ptr_BE[i] << std::endl;
-//
-//        for (int i = 0; i < cCount * oriVCount; i++)
-//                ptr_W[i] = i*10;
-//
-//        for (int i = 0; i < cCount * oriVCount; i++)
-//            std::cout << "ptr_W[" << i << "] = " << ptr_W[i] << std::endl;
-//
-//        return 0;
-//    }
 }
